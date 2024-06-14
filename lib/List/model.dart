573bb34e50d6c25d7d4444c4589d9561e0db2893
artemizaak <izaak.artem@yandex.ru>
2024-06-14 20:43:15 +0200

    Добавил наполнение для карточек

--- lib/List/model.dart
+++ lib/List/model.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
 
-class itemList extends StatefulWidget {
-  const itemList({super.key});
+class ItemList extends StatefulWidget {
+  const ItemList({super.key});
 
   @override
-  State<itemList> createState() => _itemListState();
+  State<ItemList> createState() => _itemListState();
 }
 
-class _itemListState extends State<itemList> {
+class _itemListState extends State<ItemList> {
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
       child: Container(
-        height: 80,
         width: double.infinity,
         decoration: BoxDecoration(
           color: Colors.white,
@@ -25,6 +24,30 @@ class _itemListState extends State<itemList> {
               blurRadius: 10,
             )
           ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Row(
+            children: [
+              Container(
+                width: 20,
+                height: 20,
+                decoration: const BoxDecoration(
+                  color: Colors.grey,
+                ),
+              ),
+              const SizedBox(width: 24),
+              const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Заголовок'),
+                  Text('Описание'),
+                ],
+              ),
+              const Spacer(),
+              const Text('32,12'),
+            ],
+          ),
         ),
       ),
     );
--- lib/screen.dart
+++ lib/screen.dart
@@ -21,8 +21,8 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         child: ListView(
           children: const [
-            itemList(),
-            itemList(),
+            ItemList(),
+            ItemList(),
           ],
         ),
       ),

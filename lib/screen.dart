5e3739bc04fd3989db205e2be90bfb00346bb0f5
artemizaak <izaak.artem@yandex.ru>
2024-06-26 10:46:36 +0200

    Add file Screen.dart

--- lib/screen.dart
+++ lib/screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:task_1/List/itemList.dart';
-import 'package:task_1/List/model.dart';
-import 'List/repository.dart';
+import 'List/itemList.dart';
+import 'List/controller.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,20 +11,22 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
-  CoinsModel coins = CoinsModel(clients: []);
-
-  @override
-  void initState() {
-    super.initState();
-
-    ApiServices().getCoinsModel().then(
-          (value) => setState(() => coins = value ?? coins),
-    );
-  }
+  // CoinsModel coins = CoinsModel(clients: []);
+  //
+  // @override
+  // void initState() {
+  //   super.initState();
+  //
+  //   ApiServices().getCoinsModel().then(
+  //         (value) => setState(() => coins = value ?? coins),
+  //       );
+  // }
 
   @override
   Widget build(BuildContext context) {
+    final coinsNotifier = Provider.of<CoinsNotifier>(context);
+    final coins = coinsNotifier.coins;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color.fromRGBO(246, 247, 248, 1),
@@ -33,13 +35,19 @@ class _HomeScreenState extends State<HomeScreen> {
         decoration: const BoxDecoration(
           color: Color.fromRGBO(246, 247, 248, 1),
         ),
-        child: ListView.builder(
-          cacheExtent: 300,
-          itemCount: coins.clients.length,
-          itemBuilder: (BuildContext context, int index) {
-            final client = coins.clients[index];
-            return ItemList(client: client);
-          },
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                cacheExtent: 300,
+                itemCount: coins.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final client = coins[index];
+                  return ItemList(clients: client);
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );

ad58c0106b85b0850006a7c062881f71c98f685e
pack_dev <i.pack.dev@gmail.com>
2024-06-27 13:17:24 +0300

    fix: `Client` model

--- lib/List/model.dart
+++ lib/List/model.dart
@@ -1,24 +1,4 @@
-// class Clients {
-//   List<Client> clients = [];
-//
-//   Clients({required this.clients});
-//
-//   Clients.fromJson(Map<String, dynamic> json) {
-//     clients = [
-//       for (final clientJson in json['data'] ?? []) Client.fromJson(clientJson),
-//     ];
-//   }
-//
-//   Map<String, dynamic> toJson() {
-//     final Map<String, dynamic> data = new Map<String, dynamic>();
-//     if (this.clients != null) {
-//       data['clients'] = this.clients!.map((v) => v.toJson()).toList();
-//     }
-//     return data;
-//   }
-// }
-
-  class Client {
+class Client {
   String? id;
   String? rank;
   String? symbol;
@@ -31,46 +11,47 @@
   String? changePercent24Hr;
   String? vwap24Hr;
 
-  Client(
-  {this.id,
-  this.rank,
-  this.symbol,
-  this.name,
-  this.supply,
-  this.maxSupply,
-  this.marketCapUsd,
-  this.volumeUsd24Hr,
-  this.priceUsd,
-  this.changePercent24Hr,
-  this.vwap24Hr});
+  Client({
+    this.id,
+    this.rank,
+    this.symbol,
+    this.name,
+    this.supply,
+    this.maxSupply,
+    this.marketCapUsd,
+    this.volumeUsd24Hr,
+    this.priceUsd,
+    this.changePercent24Hr,
+    this.vwap24Hr,
+  });
 
   Client.fromJson(Map<String, dynamic> json) {
-  id = json['id'];
-  rank = json['rank'];
-  symbol = json['symbol'];
-  name = json['name'];
-  supply = json['supply'];
-  maxSupply = json['maxSupply'];
-  marketCapUsd = json['marketCapUsd'];
-  volumeUsd24Hr = json['volumeUsd24Hr'];
-  priceUsd = json['priceUsd'];
-  changePercent24Hr = json['changePercent24Hr'];
-  vwap24Hr = json['vwap24Hr'];
+    id = json['id'];
+    rank = json['rank'];
+    symbol = json['symbol'];
+    name = json['name'];
+    supply = json['supply'];
+    maxSupply = json['maxSupply'];
+    marketCapUsd = json['marketCapUsd'];
+    volumeUsd24Hr = json['volumeUsd24Hr'];
+    priceUsd = json['priceUsd'];
+    changePercent24Hr = json['changePercent24Hr'];
+    vwap24Hr = json['vwap24Hr'];
   }
 
   Map<String, dynamic> toJson() {
-  final Map<String, dynamic> data = new Map<String, dynamic>();
-  data['id'] = this.id;
-  data['rank'] = this.rank;
-  data['symbol'] = this.symbol;
-  data['name'] = this.name;
-  data['supply'] = this.supply;
-  data['maxSupply'] = this.maxSupply;
-  data['marketCapUsd'] = this.marketCapUsd;
-  data['volumeUsd24Hr'] = this.volumeUsd24Hr;
-  data['priceUsd'] = this.priceUsd;
-  data['changePercent24Hr'] = this.changePercent24Hr;
-  data['vwap24Hr'] = this.vwap24Hr;
-  return data;
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['rank'] = rank;
+    data['symbol'] = symbol;
+    data['name'] = name;
+    data['supply'] = supply;
+    data['maxSupply'] = maxSupply;
+    data['marketCapUsd'] = marketCapUsd;
+    data['volumeUsd24Hr'] = volumeUsd24Hr;
+    data['priceUsd'] = priceUsd;
+    data['changePercent24Hr'] = changePercent24Hr;
+    data['vwap24Hr'] = vwap24Hr;
+    return data;
   }
-  }
+}

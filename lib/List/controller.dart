0162ba42d32f6f43a6aa3fd9035a6086fc1a0a19
pack_dev <i.pack.dev@gmail.com>
2024-06-27 14:04:30 +0300

    doc: todo-comments

--- lib/List/controller.dart
+++ lib/List/controller.dart
@@ -12,15 +12,23 @@ class CoinsNotifier extends ChangeNotifier {
     isLoading = true;
     notifyListeners();
 
-    _coins = await getClientCoin();
+    _coins = await _fetchCoins();
 
     isLoading = false;
     notifyListeners();
   }
 
-  Future<List<Coin>> getClientCoin() async {
+  Future<List<Coin>> _fetchCoins() async {
     final response = await ApiServices().getCoins();
 
     return response;
   }
 }
+
+/// Todo
+/// - детально просмотреть, как шаг за шагом работают все вызовы. Это можно узнать
+/// с помощью логирования (log или print)
+/// - немного освоить графический git в AS
+/// - подумать над тем, как связать isLoading с виджетами
+/// - разобраться с _refresh, который не вызывает обновления на самом деле
+/// - посмотреть на sealed классы и подумать над организацией состояний данные/загрузка/ошибка
